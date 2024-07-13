b509930f52842edec12d39c3a39bd8c357e7c442
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2024-07-12 23:17:39 -0700

    updated link for caption

--- src/Post.js
+++ src/Post.js
@@ -22,8 +22,8 @@ function Post() {
         const res=await axiosInstance.post("/posts/",formData)
         if(selectedFile){
           const image=res.data.image
-          //const res2=await axios.post("http://localhost:9000/caption/",{"image":image});
-          //await axiosInstance.patch("/caption/"+res.data.pk+"/",{"caption":res2.data.caption})
+          const res2=await axios.post("https://butterflyai.netlify.app/caption/",{"image":image});
+          await axiosInstance.patch("/caption/"+res.data.pk+"/",{"caption":res2.data.caption})
         }
         navigate('/posts_page')      
       };
